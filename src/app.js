e1f0f9e89d25820af2ad291cb5f8794c6efa95f6
AarushThapa <aarushthapa2@gmail.com>
2021-05-19 12:40:44 +0545

    setup

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => (
+    <>
+    Hello
+    </>
+  )
+  
+
+export default App;
+
--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app.js';
 
-const App = () => (
-  <h1
-    style={{
-      fontFamily: 'sans-serif',
-      display: 'flex',
-      height: '100vh',
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}
-  >
-    alp<span style={{ color: '#E63946' }}>ine.</span>
-  </h1>
-)
 
 ReactDOM.render(<center>{App()}</center>, document.getElementById('root'))
