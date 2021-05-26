bff37da7b0a047e111702171578e6f67ebac4dc6
Sameer Khan <contact@mdsameer.com.np>
2021-05-26 15:01:21 +0545

    feat(page): 404 Not Found

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 import DashboardView from './components/Dashboard'
 import Login from './components/Login'
 import MeetingLog from './components/Meeting/Log'
+import PageNotFound from './components/404'
 
 const App = () => {
 	console.log('one')
@@ -15,6 +16,8 @@ const App = () => {
 				<Route path="/" exact component={Login} />
 				<Route path="/dashboard" component={DashboardView} />
 				<Route path="/meeting-log" component={MeetingLog} />
+				<Route path="/page-not-found" component={PageNotFound} />
+				<Redirect to="/page-not-found" />
 			</Switch>
 		</Router>
 
